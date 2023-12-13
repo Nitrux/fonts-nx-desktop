e4b44bff96f74d4131f4d6b2f7a0bc45332b8c5c
UriHerrera <hello@uriherrera.com>
2023-12-13 16:34:08 -0600

    update pkg

--- gh-build.sh
+++ gh-build.sh
@@ -1,28 +1,16 @@
-#!/bin/bash
+#! /bin/bash
 
 set -x
 
-apt -qq update
-apt -qq -yy install equivs curl git wget gnupg2
-
-### FIXME - the container mauikit/ubuntu-18.04-amd64 does have the neon repo but for some idiotic reason it isn't working here
-
-wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
-
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
-	55751E5D > /dev/null
-
-apt -qq update
+### Basic Packages
+apt -qq -yy install equivs git devscripts lintian --no-install-recommends
 
 ### Install Dependencies
-
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules appstream
-
 mk-build-deps -i -t "apt-get --yes" -r
 
 ### Build Deb
-
-mkdir source
-mv ./* source/ # Hack for debuild
-cd source
 debuild -b -uc -us
+
+### Move Deb to current directory because debuild decided
+### that it was a GREAT IDEA TO PUT THE FILE ONE LEVEL ABOVE
+mv ../*.deb .
